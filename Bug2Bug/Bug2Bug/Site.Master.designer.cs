55ff04a48373c2ac093dc0215b1efe3e47104e04
joaquingonz7 <joaquingonz.7@gmail.com>
2014-04-30 22:09:46 -0700

    Fix Messy Merge

--- Bug2Bug/Bug2Bug/Site.Master.designer.cs
+++ Bug2Bug/Bug2Bug/Site.Master.designer.cs
@@ -55,6 +55,15 @@ namespace Bug2Bug {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
+        //protected global::System.Web.UI.HtmlControls.HtmlAnchor A4;
+        
+        /// <summary>
+        /// A4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlAnchor A4;
         
         /// <summary>
